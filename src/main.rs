eca544bc2434637b34cdc15546d9f3e278238754
hamzah abalfatah alfauzi <hamzahabalfatahalfauzi@hamzahs-MacBook-Air.local>
2025-04-04 19:10:33 -0700

    add input validation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::{cmp::Ordering, io};
-
+use std::cmp::Ordering;
+use std::io;
 use rand::Rng;
 
 fn main() {
@@ -17,8 +17,13 @@ fn main() {
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
-        let guess: u32 = guess.trim().parse().expect("input number");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
         println!("You guessed: {guess}");
+
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("too small"),
             Ordering::Equal => {
